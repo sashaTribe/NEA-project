e0cabce51dca5be032a4b2781fd039ea08321498
sashaTribe <53623386+sashaTribe@users.noreply.github.com>
2019-11-29 18:13:31 +0000

    changed number of values to be inserted
    only taking critical values up to 10% significance level because I want to see if this website would work with one-tailed test first rather than doing everything in one go. Also, I have added a comment summarising that.

--- databases.py
+++ databases.py
@@ -100,8 +100,9 @@ def insert_critical_values():
         cursor.execute(sql)
         result = cursor.fetchone()
         df = result[0]
+        # 6 question marks holding records from 6 fields listed above
         sql = "INSERT INTO chiSquare_Distribution(df, sig0_5,sig1,sig2_5,sig5,sig10)
-                VALUES (?,?,?,?,?,?,?,?,?,?);"
+                VALUES (?,?,?,?,?,?);"
 
         # records of critical values depending in the degrees of freedom on the first field
         records = [(1,2.706, 3.841, 5.024, 6.635, 10.828),
